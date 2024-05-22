33c5a90a1cdf443e83a30b21f708d0888e8b03e4
Miyashooooo <cstrike.asia@gmail.com>
2024-05-23 00:43:38 +0800

    fix: setting page realtime station

--- src/js/setting.js
+++ src/js/setting.js
@@ -111,11 +111,10 @@ Location.addEventListener("click", function() {
 });
 
 
-// 點擊選擇事件
+// 點擊選項事件
 const addLocationSelectEvent = (itemsContainer, selectElement) => {
   itemsContainer.addEventListener("click", (event) => {
     const closestDiv = event.target.closest(".usr-location .select-items > div");
-    console.log(closestDiv);
     if (closestDiv) {
       const selectedText = closestDiv.textContent;
       selectElement.textContent = selectedText;
@@ -126,7 +125,7 @@ const addLocationSelectEvent = (itemsContainer, selectElement) => {
   });
 };
 
-// 更新目前選擇的city、town
+// 更新目前選項的city、town
 const updateLocationSelectItems = (itemsContainer, items) => {
   itemsContainer.innerHTML = "";
   items.forEach(item => {
@@ -150,7 +149,7 @@ localItems.addEventListener("click", (event) => {
     localSelect.textContent = selectedLocal;
     updateLocationSelectItems(CityItems, localArr[selectedLocal]);
     updateLocationSelectItems(TownItems, []);
-    saveSelectionToLocalStorage("", "");
+    saveSelectionToLocalStorage("", "", "");
   }
 });
 
@@ -162,7 +161,7 @@ CityItems.addEventListener("click", (event) => {
     CitySelect.textContent = selectedCity;
     updateLocationSelectItems(TownItems, cityToTowns[selectedCity] || []);
     TownSelect.textContent = "town";
-    saveSelectionToLocalStorage(selectedCity, "");
+    saveSelectionToLocalStorage(selectedCity, "", "");
   }
 });
 
@@ -174,7 +173,7 @@ TownItems.addEventListener("click", (event) => {
     TownSelect.textContent = selectedTown;
     document.querySelector(".current-city").textContent = CitySelect.textContent;
     document.querySelector(".current-town").textContent = selectedTown;
-    saveSelectionToLocalStorage(CitySelect.textContent, selectedTown);
+    saveSelectionToLocalStorage(CitySelect.textContent, selectedTown, "");
   }
 });
 
@@ -212,25 +211,24 @@ document.addEventListener("mousemove", (event) => {
     sliderThumb.style.left = `${percentage}%`;
     sliderTrack.style.width = `${percentage}%`;
     settingWrapper.style.backdropFilter = `blur(${blurValue}px)`;
-
-    console.log("Slider value:", percentage);
   }
 });
 
-// 儲存user選擇的城市和城鎮到storage
-const saveSelectionToLocalStorage = (city, town) => {
+// 儲存user選擇的city和town到storage
+const saveSelectionToLocalStorage = (city, town, station) => {
   localStorage.setItem("current-city", city);
   localStorage.setItem("current-town", town);
+  localStorage.setItem("current-station", station);
 };
 
-// 從storage儲存中取得user之前保存的選擇
+// 從storage儲存中取得user之前保存的選項
 const getSelectionFromLocalStorage = () => {
   const city = localStorage.getItem("current-city");
   const town = localStorage.getItem("current-town");
   return { city, town };
 };
 
-// 渲染user之前保存的選擇到頁面
+// 渲染user之前保存的選項到頁面
 const renderSelectionFromLocalStorage = () => {
   const { city, town } = getSelectionFromLocalStorage();
   if (town) {
@@ -240,75 +238,114 @@ const renderSelectionFromLocalStorage = () => {
   }
 };
 
-// 渲染user之前保存的選擇
+// 渲染user之前保存的選項
 window.addEventListener("DOMContentLoaded", renderSelectionFromLocalStorage);
 
 const StationWrapper = document.querySelector(".realtime-station");
 const StationLocation = StationWrapper.querySelector(".location");
 const StationSelectWrapper = StationWrapper.querySelector(".select-wrapper");
+const StationLocalSelect = StationSelectWrapper.querySelector(".current-local");
+const StationLocalItems = StationSelectWrapper.querySelector(".local");
 const StationSelect = StationSelectWrapper.querySelector(".current-station");
 const StationItems = StationSelectWrapper.querySelector(".station");
+const StationList = [];
+const StationRegion = [];
 
 // 取得即時測站
-function realtime_station() {
-  let retryCount = 0;
-  const retryClock = setInterval(async () => {
-    retryCount++;
+async function realtime_station() {
+  try {
+    const res = await fetchData(`${API_url()}v1/trem/station`);
+    const data = await res.json();
 
-    try {
-      const res = await fetchData(`${API_url()}v1/trem/station`);
-      const data = await res.json();
+    if (data) {
+      StationItems.innerHTML = "";
+      const stationsArray = Object.keys(data).map(station => {
+        const info = data[station].info[data[station].info.length - 1];
+        let loc = region_code_to_string(constant.REGION, info.code);
 
-      if (data) {
-        StationItems.innerHTML = "";
-        const stationsArray = Object.keys(data).map(station => {
-          const info = data[station].info[data[station].info.length - 1];
-          let loc = region_code_to_string(constant.REGION, info.code);
+        // 將 loc.city 添加到 StationRegion 數組中
+        if (loc && !StationRegion.includes(loc.city))
+          StationRegion.push(loc.city);
 
-          if (!loc)
-            if (station === "13379360")
-              loc = "重慶市北碚區";
-            else if (station === "7735548")
-              loc = "南陽州市和道邑";
-            else
-              loc = "未知區域";
+
+        if (!loc)
+          if (station === "13379360")
+            loc = "重慶市北碚區";
+          else if (station === "7735548")
+            loc = "南陽州市和道邑";
           else
-            loc = `${loc.city}${loc.town}`;
+            loc = "未知區域";
+        else
+          loc = `${loc.city}${loc.town}`;
 
-          return {
-            name : station,
-            net  : data[station].net,
-            loc  : loc,
-            code : info.code,
-          };
-        });
+        const StationInfo = {
+          name : station,
+          net  : data[station].net,
+          loc  : loc,
+          code : info.code,
+        };
 
-        stationsArray.sort((b, a) => a.loc.localeCompare(b.loc));
+        StationList.push(StationInfo);
+        return StationInfo;
+      });
 
-        stationsArray.forEach(station => {
-          const stationDiv = document.createElement("div");
-
-          const netSpan = document.createElement("span");
-          netSpan.textContent = station.net;
-
-          const infoSpan = document.createElement("span");
-          infoSpan.textContent = `${station.code}-${station.name} ${station.loc}`;
-
-          stationDiv.appendChild(netSpan);
-          stationDiv.appendChild(infoSpan);
-
-          StationItems.appendChild(stationDiv);
-        });
-
-        clearInterval(retryClock);
-      }
-    } catch (err) {
-      logger.error(`[Fetch] ${err} (Try #${retryCount})`);
+      RenderStationRegion();
     }
-  }, 500);
+  } catch (err) {
+    logger.error(`[Fetch] ${err}`);
+  }
+}
+realtime_station();
+
+function RenderStationRegion() {
+  StationLocalItems.innerHTML = "";
+
+  const sortedRegion = StationRegion.sort();
+
+  sortedRegion.forEach(city => {
+    const truncatedCity = city.slice(0, -1);
+
+    const cityDiv = document.createElement("div");
+    cityDiv.textContent = truncatedCity;
+    StationLocalItems.appendChild(cityDiv);
+  });
 }
 
-realtime_station();
+// 即時測站-點擊縣市選項事件
+StationLocalItems.addEventListener("click", (event) => {
+  const target = event.target;
+  if (target.tagName === "DIV") {
+    StationLocalItems.querySelectorAll("div").forEach(div => div.classList.remove("select-option-selected"));
+
+    target.classList.add("select-option-selected");
+
+    const selectedCity = target.textContent;
+    const filteredStations = StationList.filter(station => station.loc.includes(selectedCity));
+    renderFilteredStations(filteredStations);
+  }
+});
+
+// 即時測站-篩選對應縣市測站並排序後渲染
+function renderFilteredStations(stations) {
+  stations.sort((a, b) => a.loc.localeCompare(b.loc));
+
+  StationItems.innerHTML = "";
+
+  stations.forEach(station => {
+    const stationDiv = document.createElement("div");
+    const netSpan = document.createElement("span");
+    netSpan.textContent = station.net;
+    netSpan.classList.add(station.net);
+
+    const infoSpan = document.createElement("span");
+    infoSpan.textContent = `${station.code}-${station.name} ${station.loc}`;
+
+    stationDiv.appendChild(netSpan);
+    stationDiv.appendChild(infoSpan);
+
+    StationItems.appendChild(stationDiv);
+  });
+}
 
 // 即時測站-下拉選單點擊事件
 StationLocation.addEventListener("click", function() {
@@ -320,7 +357,7 @@ StationLocation.addEventListener("click", function() {
   StationSelectWrapper.classList.toggle("select-show-big");
 });
 
-// 即時測站-點擊選擇事件
+// 即時測站-點擊測站選項事件
 const addStationSelectEvent = (itemsContainer) => {
   itemsContainer.addEventListener("click", (event) => {
     const closestDiv = event.target.closest(".realtime-station .select-items > div");
@@ -333,10 +370,10 @@ const addStationSelectEvent = (itemsContainer) => {
       const regex = /^(MS-Net|SE-Net)(\d+-\d+.*)$/;
       const match = selectedStation.match(regex);
       if (match) {
-        const Net = match[1];
-        const StationInfo = match[2];
-        StationSelect.textContent = `${Net} ${StationInfo}`;
-        document.querySelector(".current-station").textContent = `${Net} ${StationInfo}`;
+        StationSelect.textContent = `${match[1]} ${match[2]}`;
+        document.querySelector(".current-station").textContent = `${match[1]} ${match[2]}`;
+        saveSelectionToLocalStorage(getSelectionFromLocalStorage().city, getSelectionFromLocalStorage().town, `${match[1]} ${match[2]}`);
+        console.log(StationRegion);
       }
     }
   });
